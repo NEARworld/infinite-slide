d15bc69ecfb62c75220e020b69dd714c04b358ba
nearworld <artemismars2@outlook.com>
2023-12-04 10:59:15 +0900

    feat: Slider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { VisibleArea } from "./components/VisibleArea";
+import { Slider } from "./components/Slider";
 
 function App() {
   return (
     <VisibleArea width={1200} height={400}>
-      <div>Slide</div>
+      <Slider />
     </VisibleArea>
   );
 }
---
+++ src/components/Slider.tsx
@@ -0,0 +1,11 @@
+export const Slider = () => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 1fr)",
+        transition: "all 1s ease-in-out",
+      }}
+    ></div>
+  );
+};
